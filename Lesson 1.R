48c53fdb9ce3b571313651b3cef22731ee47bce9
Martin Košatka <82899462+Elladan1337@users.noreply.github.com>
2024-03-02 20:12:44 +0100

    Finished part 1 of Lesson 1

--- Lesson 1.R
+++ Lesson 1.R
@@ -27,3 +27,99 @@ View(pirates)
 # Documentation
 ?pirates
 
+# What is the mean age?
+mean(pirates$age)
+
+# What was the tallest pirate?
+max(pirates$height)
+
+# How many pirates are there of each sex?
+table(pirates$sex)
+
+# Calculate the mean age, separately for each sex
+aggregate(x = age ~ sex,
+          data = pirates,
+          FUN = mean)
+
+# But should it not be gender instead?
+# What to do?
+colnames(pirates)[colnames(pirates) == "sex"] <- "gender"
+View(pirates)
+# WHAT?!
+# This is the code that you can find by google (or generated)
+# But you don't understand it, which is a problem - if anything goes wrong
+
+
+# Let's see what we've got ------------------------------------------------
+
+# Create a basic scatterplot
+plot(x = pirates$height,        # X coordinates
+     y = pirates$weight)        # y-coordinates
+
+# Create a nicer scatterplot
+plot(x = pirates$height,        # X coordinates
+     y = pirates$weight,        # y-coordinates
+     main = 'My first scatterplot of pirate data!',
+     xlab = 'Height (in cm)',   # x-axis label
+     ylab = 'Weight (in kg)',   # y-axis label
+     pch = 16,                  # Filled circles
+     col = gray(.0, .1))        # Transparent gray
+
+# Create a very nice scatterplot
+plot(x = pirates$height,        # X coordinates
+     y = pirates$weight,        # y-coordinates
+     main = 'My first scatterplot of pirate data!',
+     xlab = 'Height (in cm)',   # x-axis label
+     ylab = 'Weight (in kg)',   # y-axis label
+     pch = 16,                  # Filled circles
+     col = gray(.0, .1))        # Transparent gray
+
+grid()        # Add gridlines
+
+# Create a linear regression model
+model <- lm(formula = weight ~ height, 
+            data = pirates)
+
+abline(model, col = 'blue')      # Add regression to plot
+
+# Create a kind of boxplot called "Pirateplot"
+pirateplot(formula = age ~ sword.type, 
+           data = pirates,
+           main = "Pirateplot of ages by favorite sword")
+
+pirateplot(formula = height ~ gender,               # Plot weight as a function of gender
+           data = pirates,                       
+           main = "Pirateplot of height by gender",
+           pal = "pony",                         # Use the info color palette
+           theme = 3)                            # Use theme 3
+
+# Into hypothesis testing -------------------------------------------------
+
+# Age by headband t-test
+t.test(formula = age ~ headband,
+       data = pirates,
+       alternative = 'two.sided')
+
+#Height - Weight correlation test
+cor.test(formula = ~ height + weight,
+         data = pirates)
+
+
+# Create tattoos model
+tat.sword.lm <- lm(formula = tattoos ~ sword.type,
+                   data = pirates)
+
+# Get ANOVA table
+anova(tat.sword.lm)
+
+# Create a linear regression model: DV = tchests, IV = age, weight, tattoos
+tchests.model <- lm(formula = tchests ~ age + weight + tattoos,
+                    data = pirates)
+
+# Show summary statistics
+summary(tchests.model)
+
+
+# Part 2. Where we start comprehending what we just did -------------------
+
+
