db03d9af99e7e6e9155d832034c7203a024f0a39
Martin Košatka <82899462+Elladan1337@users.noreply.github.com>
2025-03-23 21:54:38 +0100

    Create Lesson 5.R

---
+++ Lesson 5.R
@@ -0,0 +1,6 @@
+#So far, we have only been looking at nice datasets from libraries
+#How does this compare to real life? It depends...
+
+# Let's look at two datasets
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2F00025429%2F1493899967
+# https://data.gov.cz/datov%C3%A1-sada?iri=https%3A%2F%2Fdata.gov.cz%2Fzdroj%2Fdatov%C3%A9-sady%2F00025429%2F1493900249
